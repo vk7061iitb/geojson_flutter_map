708cf95eb87dbde9f12302ba584653bb1293e809
Vikash Kumar <vk4097440@gmail.com>
2024-03-12 23:03:06 +0530

    This is a new commit for what I originally planned to be amended

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 # geojson_flutter_map
+=======
+# geojson_flutter_map
+
+A new Flutter project.
+>>>>>>> f72a247 (Initial Commit)
---
+++ android/app/src/main/kotlin/com/example/geojson_flutter_map/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.geojson_flutter_map
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
